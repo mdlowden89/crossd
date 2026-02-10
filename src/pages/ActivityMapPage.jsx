de5a0656e8b733874d444f4fa4a70cbe3ea3fdca
Matt Lowden <mattdavidlowden89@gmail.com>
2026-02-10 22:08:22 +0000

    File changes

--- src/pages/ActivityMapPage.jsx
+++ src/pages/ActivityMapPage.jsx
@@ -39,10 +39,10 @@ export default function ActivityMapPage() {
     <div className="fixed inset-0 bg-black z-[999] flex flex-col safe-area-top safe-area-bottom">
       {/* Close button */}
       <button
-        onClick={() => navigate(-1)}
-        className="absolute top-3 right-3 md:top-4 md:right-4 z-50 p-2 rounded-full bg-black/70 hover:bg-black/90 transition-colors"
+        onClick={() => navigate(createPageUrl('Dashboard'))}
+        className="absolute top-4 right-4 z-50 p-2 rounded-full bg-black/70 hover:bg-black/90 transition-colors"
       >
-        <X className="w-5 h-5 md:w-6 md:h-6 text-white" />
+        <X className="w-6 h-6 text-white" />
       </button>
 
       {/* Map - fills most of screen */}
